3d55dc6a0d3a00c938ba0d65302006fad6535b3f
Zachary Waweru <actuaryzask@gmail.com>
2021-09-06 19:12:56 +0300

    added work.courzehub.com to allowed hosts 3

--- server/courzehub_work/settings/dev.py
+++ server/courzehub_work/settings/dev.py
@@ -16,7 +16,7 @@ SITE_NAME = config('SITE_NAME')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['work.courzehub.com']
 
 CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:8000',
                          'http://localhost:3000', 'http://localhost:8000', 'http://127.0.0.1:3000')
