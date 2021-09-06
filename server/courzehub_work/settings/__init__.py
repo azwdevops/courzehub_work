6f42af2e928e64e03182e4590fce8536522ff631
Zachary Waweru <actuaryzask@gmail.com>
2021-09-06 11:48:39 +0300

    deleting migrations and running them again

--- server/courzehub_work/settings/__init__.py
+++ server/courzehub_work/settings/__init__.py
@@ -6,7 +6,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-PRODUCTION = True
+PRODUCTION = False
 
 # production settings
 if PRODUCTION:
